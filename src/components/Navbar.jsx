b98efb4d57c6d8dbc2c8410f0cbf5513e134292e
jawadahmed554 <ahmed.jawad554@gmail.com>
2022-06-26 00:41:51 +0500

    boilerplate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+    color: white;
+`
+
+const Navbar = () => {
+  return (
+    <Container>Navbar Ready</Container>
+  )
+}
+
+export default Navbar
